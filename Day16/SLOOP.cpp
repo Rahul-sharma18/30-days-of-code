5751763caf5f7a1069554395b28e9ce1a320bd3c
Rahul-sharma18 <98330979+Rahul-sharma18@users.noreply.github.com>
2022-05-10 21:33:36 +0530

    Create SLOOP.cpp

---
+++ Day16/SLOOP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	       int m, s;
+	       cin>>m>>s;
+	       std::cout << m/s << std::endl;
+	       
+	}
+	return 0;
+}
